6658d19ad6ed8075c2ec4ff4d900866037a6023f
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2025-02-28 08:24:58 +0530

    Added capacitor android support

---
+++ android/app/src/main/java/com/example/foodelivery/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.foodelivery;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.foodelivery',
+  appName: 'Food Delivery',
+  webDir: 'dist'
+};
+
+export default config;
